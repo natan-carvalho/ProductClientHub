9b6306ec67bb084187b722380221cc0ae92ae6a0
natancs <natan.carvalho123@hotmail.com>
2025-09-01 12:56:18 +0000

    :wrench: chore: configured the connection with data source

---
+++ ProductClientHub.API/Infrastructure/ProductClientHubDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using ProductClientHub.API.Entities;
+
+namespace ProductClientHub.API.Infrastructure;
+
+public class ProductClientHubDbContext : DbContext
+{
+  public DbSet<Client> Clients { get; set; } = default!;
+  public DbSet<Product> Products { get; set; } = default!;
+
+  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+  {
+    optionsBuilder.UseSqlite("Data Source=../../ProductClientHubDB.db");
+  }
+}

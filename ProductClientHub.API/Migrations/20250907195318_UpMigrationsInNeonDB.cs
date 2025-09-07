233afc9dcf24c1e4cf507f2e38da645ce110f90e
natan-carvalho <natan.carvalho123@hotmail.com>
2025-09-07 17:00:20 -0300

    :wrench: chore: configured Databas int Neon.tech

---
+++ ProductClientHub.API/Migrations/20250907195318_UpMigrationsInNeonDB.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProductClientHub.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpMigrationsInNeonDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
